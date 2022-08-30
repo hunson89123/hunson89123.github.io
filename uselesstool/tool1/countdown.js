d9bfbd645c8aea4ff7096b9749b81666a1222775
hunson89123 <hunson89123@gmail.com>
2022-08-30 17:13:47 +0800

    some change...

--- uselesstool/tool1/countdown.js
+++ uselesstool/tool1/countdown.js
@@ -1,7 +1,7 @@
 var today = new Date();
 var nxtYear = today.getFullYear()+1;
 var happyNewYear = new Date("Jan 1, "+ nxtYear +" 00:00:00")
-document.getElementById('title').innerHTML = "距離 "+ nxtYear + " 年剩下";
+document.getElementById('title').textContent = "距離 "+ nxtYear + " 年剩下";
 var x = setInterval(function(){
   var now = new Date().getTime();
   var cuntTime = happyNewYear - now;
@@ -9,8 +9,8 @@ var x = setInterval(function(){
   var hours = Math.floor((cuntTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((cuntTime % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((cuntTime % (1000 * 60)) / 1000);
-  document.getElementById('days').innerHTML = days;
-  document.getElementById('hours').innerHTML = hours;
-  document.getElementById('minutes').innerHTML = minutes;
-  document.getElementById('seconds').innerHTML = seconds;
+  document.getElementById('days').textContent = days;
+  document.getElementById('hours').textContent = hours;
+  document.getElementById('minutes').textContent = minutes;
+  document.getElementById('seconds').textContent = seconds;
 },0);
