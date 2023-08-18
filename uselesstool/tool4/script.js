1fd3dcc6865c355d2c6b35c5180a6691e5391ab0
hunson89123 <hunson89123@gmail.com>
2023-08-18 09:28:08 +0800

    2023/08/18 週五  9:28:08.05

--- uselesstool/tool4/script.js
+++ uselesstool/tool4/script.js
@@ -1,26 +1,41 @@
-const showSelect = document.getElementById('select');
-const showResult = document.getElementById('result');
-var point = 0;
-var totle = 0;
-function selected(btn) {
-    if (btn.id == 'A') point = 20;
-    else if (btn.id == '9' || btn.id == 'J' || btn.id == 'Q' || btn.id == 'K') point = 10;
-    else point = parseInt(btn.id);
-    showSelect.innerHTML = "<span class='text-danger'>" + btn.id + "</span> 加" + point + "分";
-}
-function plus() {
-    totle += point;
-    point = 0;
-    update();
+const sheetId = '1n3A5v4pxKZ6yzytGDUezmls5VeLVPrFyX8qF5QTtCKg';
+const base = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?`;
+const query = encodeURIComponent('Select *');
+const sheetName = ['room'];
+var url = `${base}&sheet=${sheetName[0]}&tq=${query}`
+fetch(url)
+    .then(res => res.text())
+    .then(rep => {
+        //Remove additional text and extract only JSON:
+        const jsonData = JSON.parse(rep.substring(47).slice(0, -2));
+        console.log(jsonData)
+    })
+function genRoomId() {
+    var roomId = "";
+    for (var x = 0; x < 4; x++) {
+        var idStr = Math.floor(Math.random() * 10) + '';
+        roomId += idStr;
+    }
+    return roomId;
 }
 
-function clr() {
-    point = 0;
-    totle = 0;
-    update();
-}
+function createRoom(b) {
+    b.innerHTML = genRoomId();
 
-function update() {
-    showResult.innerHTML = "<b style='font-size: 100px'>" + totle + "</b>";
-    showSelect.innerHTML = "-";
+    var dataToUpdate = "New Value";
+    fetch('https://script.google.com/1Q0IhfpZfY0h2HTO6fo8fImJa5Ho8ur0nHJcFYLw9wTx4Aun4BPBx7PSD/exec', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: 'data=' + encodeURIComponent(dataToUpdate)
+    })
+        .then(response => response.text())
+        .then(result => {
+            console.log('Data updated:', result);
+        })
+        .catch(error => {
+            console.error('Error updating data:', error);
+        });
+    // console.log(genRoomId());
 }
---
+++ uselesstool/tool4/script_old.js
@@ -0,0 +1,26 @@
+const showSelect = document.getElementById('select');
+const showResult = document.getElementById('result');
+var point = 0;
+var totle = 0;
+function selected(btn) {
+    if (btn.id == 'A') point = 20;
+    else if (btn.id == '9' || btn.id == 'J' || btn.id == 'Q' || btn.id == 'K') point = 10;
+    else point = parseInt(btn.id);
+    showSelect.innerHTML = "<span class='text-danger'>" + btn.id + "</span> 加" + point + "分";
+}
+function plus() {
+    totle += point;
+    point = 0;
+    update();
+}
+
+function clr() {
+    point = 0;
+    totle = 0;
+    update();
+}
+
+function update() {
+    showResult.innerHTML = "<b style='font-size: 100px'>" + totle + "</b>";
+    showSelect.innerHTML = "-";
+}
