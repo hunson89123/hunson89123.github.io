0c967bf6539b50b81808443fcb84aef0544e39cf
hunson89123 <hunson89123@gmail.com>
2023-10-22 23:51:20 +0800

    2023/10/22 週日 23:51:19.66

---
+++ uselesstool/csnplus/script.js
@@ -0,0 +1,39 @@
+const eat_list = ["炒飯", "鍋燒意麵", "牛肉麵", "肉燥飯", "壽司", "定食", "速食", "拉麵", "咖哩飯", "雞肉飯", "涼麵", "水餃", "雞肉飯", "鍋貼", "豬排飯", "牛排", "火鍋"];
+const res_wrap = document.querySelector(".res_wrap");
+const start_btn = document.querySelector(".start_btn")
+var eat_res = "👇";
+//按下[開始]
+function start() {
+    if (res_wrap.classList.contains("stop")) res_wrap.classList.remove("stop");
+    start_btn.disabled = true;
+    res_wrap.ontransitionend = () => finish();
+    res_wrap.classList.add("run");
+    var shuffle_list = shuffle(eat_list);
+    res_wrap.innerHTML = "<p>" + eat_res + "</p>";
+    for (var x = 0; x < shuffle_list.length; x++) {
+        var c = document.createElement("p");
+        c.innerHTML = shuffle_list[x];
+        res_wrap.appendChild(c);
+    }
+    eat_res = shuffle_list.slice(-1);
+}
+
+//動畫結束
+function finish() {
+    if (res_wrap.classList.contains("run")) {
+        res_wrap.classList.remove("run");
+        res_wrap.classList.add("stop");
+        res_wrap.innerHTML = "<p>" + eat_res + "</p>";
+        document.querySelector(".res_wrap p").classList.add("res_text");
+        start_btn.disabled = false;
+    }
+}
+
+//YatesShuffle演算法
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
