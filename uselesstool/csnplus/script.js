d7b467da506ad34735a04a2909e4f56879a57e0d
hunson89123 <hunson89123@gmail.com>
2023-10-24 15:12:36 +0800

    2023/10/24 週二 15:12:35.72

--- uselesstool/csnplus/script.js
+++ uselesstool/csnplus/script.js
@@ -1,14 +1,17 @@
 const eat_list = ["炒飯", "鍋燒意麵", "牛肉麵", "肉燥飯", "壽司", "定食", "速食", "拉麵", "咖哩飯", "雞肉飯", "涼麵", "水餃", "雞肉飯", "鍋貼", "豬排飯", "牛排", "火鍋"];
 const res_wrap = document.querySelector(".res_wrap");
 const start_btn = document.querySelector(".start_btn")
+const locate_btn = document.querySelector(".locate_btn")
+const search_area = document.querySelector(".search_area");
 const eat_search = document.querySelector(".search_area .eat_search");
 const eat_res_search = document.querySelector("#eat_res_search");
-const search_a = document.querySelector("#search_a")
-var eat_res = "👇";
+const emo_press = "👇", emo_loading = "🔎";
+var eat_res = "🍴";
 //按下[開始]
 function start() {
     if (res_wrap.classList.contains("stop")) res_wrap.classList.remove("stop");
     start_btn.disabled = true;
+    locate_btn.disabled = true;
     eat_search.style.display = "none";
     res_wrap.ontransitionend = () => finish();
     res_wrap.classList.add("run");
@@ -22,6 +25,10 @@ function start() {
     eat_res = shuffle_list.slice(-1);
 }
 
+//搜尋附近的「食物」
+function search_eat() {
+    location.href = "https://www.google.com/maps/search/" + eat_res;
+}
 //動畫結束
 function finish() {
     if (res_wrap.classList.contains("run")) {
@@ -29,10 +36,11 @@ function finish() {
         res_wrap.classList.add("stop");
         res_wrap.innerHTML = "<p>" + eat_res + "</p>";
         eat_res_search.innerHTML = eat_res;
-        search_a.href = "https://www.google.com/maps/search/" + eat_res;
         eat_search.style.display = "flex";
         document.querySelector(".res_wrap p").classList.add("res_text");
         start_btn.disabled = false;
+        start_btn.innerText = "換一個";
+        locate_btn.disabled = false;
     }
 }
 
