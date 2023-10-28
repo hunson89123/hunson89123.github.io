3f6735b6c1ffdceb08dae0019cf5efcdd3cc8daa
hunson89123 <hunson89123@gmail.com>
2023-10-28 19:43:40 +0800

    2023/10/28 週六 19:43:39.60

--- uselesstool/csnplus/intro/script.js
+++ uselesstool/csnplus/intro/script.js
@@ -1,13 +1,42 @@
 const bg_img = document.querySelector(".bg-img");
-const title_food = document.querySelector(".title-food");
-const food_list = ["壽司", "鍋貼", "漢堡", "披薩", "炒飯", "快炒", "素食", "速食", "火鍋", "牛排", "烤肉", "刈包", "肉粽", "肉圓", "碗粿", "滷味", "便當"];
+const eatext = document.querySelector("#eatext");
+const title_wrap = document.querySelector(".title-wrap");
+const title_food_wrap = document.querySelector(".title-food-wrap");
+const title_food_1 = document.querySelector(".title-food-1");
+const title_food_2 = document.querySelector(".title-food-2");
+const title_plus = document.querySelector(".title-plus");
+const title_plus_text = document.querySelector(".title-plus span");
+const food_list = ["壽司", "鍋貼", "漢堡", "披薩", "炒飯", "快炒", "素食", "速食", "火鍋", "牛排", "烤肉", "刈包", "滷味", "便當", "拉麵"];
+const show_length = 5;
+var food_iter = 0;
 bg_img.src = "https://source.unsplash.com/random/1920x1080/?food";
-randomFoodTxt();
-title_food.onanimationiteration = () => animIter();
-function animIter() {
-    randomFoodTxt();
+bg_img.onload = function () {
+    eatext.style.display = "flex";
+    title_food_wrap.style.display = "flex";
+}
+eatext.style.color = "rgb(" + (Math.floor(Math.random() * 125) + 130) + "," + (Math.floor(Math.random() * 125) + 130) + ",0)";
+shuffle(food_list);
+for (var x = 0; x < show_length + 1; x++) {
+    var c = document.createElement("span");
+
+    if (x == 0) c.innerHTML = "　";
+    else if (x == show_length) c.innerHTML = "啥呢";
+    else c.innerHTML = food_list[x];
+    c.classList.add("title");
+    title_food_wrap.appendChild(c);
+}
+title_food_wrap.onanimationend = () => animEnd();
+
+function animEnd() {
+    title_wrap.style.animation = "textAnimLeft .5s ease-in-out forwards";
+    title_plus.style.display = "flex";
 }
 
-function randomFoodTxt() {
-    title_food.innerText = food_list[Math.floor(Math.random() * food_list.length)];
+//YatesShuffle演算法
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
 }
