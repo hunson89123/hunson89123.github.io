3dce0ee9601b0756b90d58ccbca3b2bf0043d2e1
hunson89123 <hunson89123@gmail.com>
2023-11-06 01:14:13 +0800

    2023/11/06 週一  1:14:12.87

--- scripts/title.js
+++ scripts/title.js
@@ -1,7 +1,11 @@
 const title = document.querySelector('.title');
-const title_text = "Hunson's Lab"
+let title_text = document.title;
 var count = 0;
 var delay = 87;
+//判斷標題長度，過長時則換行
+if (title_text.length > 16) {
+    title_text = title_text.replaceAll(" ", "<br>")
+}
 const interval = setInterval(function () {
     title.innerHTML = title_text
         .split("")
@@ -12,7 +16,7 @@ const interval = setInterval(function () {
 }, 50)
 
 function getRndChar(c, i) {
-    if (i < count) return title_text[i];
+    if (i < count || c == "<" || c == "b" && title_text[i - 1] == '<' || c == "r" && title_text[i + 1] == '>' || c == ">") return title_text[i];
     //非字母
     if (c.charCodeAt(0) < 65) return String.fromCharCode(32 + Math.floor(Math.random() * 16))
     //大寫字母
