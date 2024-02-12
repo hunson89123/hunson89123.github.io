d812eb837612b9da2e65e3e391d82935e366fd04
hunson89123 <hunson89123@gmail.com>
2024-02-13 00:08:09 +0800

    2024/02/13 週二  0:08:09.33

--- uselesstool/lolhelper/script.js
+++ uselesstool/lolhelper/script.js
@@ -1,5 +1,6 @@
 $('#summonerForm').submit(function (event) {
     event.preventDefault();
+    const riotAPIKey = process.env.RIOTAPIKEY;
     const search_text = $('#search_input').val().split('#');
     const gameName = search_text[0]
     const tagLine = search_text[1]
@@ -11,7 +12,7 @@ $('#summonerForm').submit(function (event) {
             "Accept-Language": "en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7,zh-CN;q=0.6",
             "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
             "Origin": "https://developer.riotgames.com",
-            "X-Riot-Token": process.env.RIOTAPIKEY
+            "X-Riot-Token": riotAPIKey
         },
         success: function (data) {
             const summonerInfo = `
