6b08281e87b10ad0a96cf710cdefeaf106667e65
hunson89123 <hunson89123@gmail.com>
2024-02-13 00:57:30 +0800

    2024/02/13 週二  0:57:30.31

--- uselesstool/lolhelper/script.js
+++ uselesstool/lolhelper/script.js
@@ -1,15 +1,29 @@
-const summonerInfo = document.getElementById('summonerInfo');
 $('#summonerForm').submit(function (event) {
     event.preventDefault();
-    // 定義一個全局函數來處理 JSONP 回調
+    const riotAPIKey = process.env.RIOTAPIKEY;
+    const search_text = $('#search_input').val().split('#');
+    const gameName = search_text[0]
+    const tagLine = search_text[1]
 
-
-    // 創建一個 <script> 標籤來發送 JSONP 請求
-    var script = document.createElement('script');
-    script.src = 'https://script.google.com/macros/s/AKfycbzRNXZEEnYfIU7HM_MmDqKueYdYgSxiTUuAjwjoIs1SzkwZhni1wy9XNn3WwHQC3E0J/exec?callback=handleData';
-    document.body.appendChild(script);
-
+    $.ajax({
+        url: `https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}?api_key=${riotAPIKey}`,
+        headers: {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+            "Accept-Language": "en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7,zh-CN;q=0.6",
+            "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+            "Origin": "https://developer.riotgames.com",
+            "X-Riot-Token": riotAPIKey
+        },
+        success: function (data) {
+            const summonerInfo = `
+                <h2>您的召喚師ID為</h2>
+                <p>${data.puuid}</p>
+            `;
+            $('#summonerInfo').html(summonerInfo);
+        },
+        error: function (x, s, error) {
+            console.log(s);
+            $('#summonerInfo').html(`<p>錯誤: ${error.responseText}</p>`);
+        }
+    });
 });
-function handleData(data) {
-    $('summonerInfo').innerText(data);
-}
