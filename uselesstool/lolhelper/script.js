65cd28260a9252ddac6533dc08c5cf43f9c0068a
hunson89123 <hunson89123@gmail.com>
2024-02-13 00:45:40 +0800

    2024/02/13 週二  0:45:40.28

--- uselesstool/lolhelper/script.js
+++ uselesstool/lolhelper/script.js
@@ -1,10 +1,15 @@
 const summonerInfo = document.getElementById('summonerInfo');
 $('#summonerForm').submit(function (event) {
     event.preventDefault();
-    fetch('https://script.google.com/macros/s/AKfycbzRNXZEEnYfIU7HM_MmDqKueYdYgSxiTUuAjwjoIs1SzkwZhni1wy9XNn3WwHQC3E0J/exec')
-        .then(response => response.text())
-        .then(data => {
-            summonerInfo.innerText = data;
-        })
-        .catch(error => summonerInfo.innerText = '錯誤:' + error);
+    // 定義一個全局函數來處理 JSONP 回調
+
+
+    // 創建一個 <script> 標籤來發送 JSONP 請求
+    var script = document.createElement('script');
+    script.src = 'https://script.google.com/macros/s/AKfycbzRNXZEEnYfIU7HM_MmDqKueYdYgSxiTUuAjwjoIs1SzkwZhni1wy9XNn3WwHQC3E0J/exec?callback=handleData';
+    document.body.appendChild(script);
+
 });
+function handleData(data) {
+    $('summonerInfo').innerText(data);
+}
