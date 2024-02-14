8933a08f97623b99148566d96456381298a69f6d
hunson89123 <hunson89123@gmail.com>
2024-02-15 00:12:07 +0800

    2024/02/15 週四  0:12:06.91

--- uselesstool/3duc/script.js
+++ uselesstool/3duc/script.js
@@ -3,50 +3,65 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, .1, 1000);
+const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, .1, 1000);
+const msg = document.getElementById('msg');
 let object;
-let controls;
 let objToRender = 'taipei101';
-let mousePos = { x: undefined, y: undefined };
+let mousePos = { x: 0, y: 0 };
 const loaders = new GLTFLoader();
 loaders.load(
-    `scene.gltf`,
+    `earth/scene.gltf`,
     function (gltf) {
         object = gltf.scene;
         scene.add(object);
+
+        /* 處理地糗軸心置中問題 */
+        var box = new THREE.Box3().setFromObject(object);
+        box.getCenter(object.position); // this re-sets the object position
+        object.position.multiplyScalar(- 1);
+
+        var pivot = new THREE.Group();
+        scene.add(pivot);
+        pivot.add(object);
     },
     function (xhr) {
-        console.log('已載入' + (xhr.loaded / xhr.total * 100) + '%')
+        msg.innerText = (xhr.loaded / xhr.total * 100) + '%'
     },
     function (error) {
-        console.log(error);
+        msg.innerText = error;
     }
 );
 
 const renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
+const controls = new OrbitControls(camera, renderer.domElement);
 
 document.getElementById("container3D").appendChild(renderer.domElement);
-camera.position.z = 150;
+camera.position.z = 15;
 
 const topLight = new THREE.DirectionalLight(0xffffff, 1);
 topLight.position.set(500, 50, 500);
 topLight.castShadow = true;
 scene.add(topLight);
 
-const ambientLight = new THREE.AmbientLight(0x333333, 5);
+const ambientLight = new THREE.AmbientLight(0x000000, 5);
 scene.add(ambientLight);
 
 
 function animate() {
     requestAnimationFrame(animate);
-
-    if (object && objToRender === "taipei101") {
-        object.rotation.y = -3 + mousePos.x / window.innerWidth * 3;
-        object.rotation.x = -1.2 + mousePos.y * 2.5 / window.innerHeight;
-    }
     renderer.render(scene, camera);
 }
+
+// 添加陀螺儀控制
+function handleOrientation(event) {
+    var x = event.beta;  // 在[-180, 180]之間，表示前後傾斜。
+    var y = event.gamma; // 在[-90, 90]之間，表示左右傾斜。
+    object.rotation.x = x * Math.PI / 180;
+    object.rotation.y = y * Math.PI / 180;
+}
+
+window.addEventListener("deviceorientation", handleOrientation, true);
 
 window.addEventListener("resize", function () {
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -54,7 +69,4 @@ window.addEventListener("resize", function () {
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-window.addEventListener('mousemove', (event) => {
-    mousePos = { x: event.clientX, y: event.clientY };
-});
 animate();
