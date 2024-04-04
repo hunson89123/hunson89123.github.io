ac28c06308487cc4717da8215e0eb21000f2ed15
hunson89123 <hunson89123@gmail.com>
2024-04-04 23:38:34 +0800

    週四 2024/04/04 23:38:33.79

--- uselesstool/tool7/script.js
+++ uselesstool/tool7/script.js
@@ -1,8 +1,22 @@
-var map = L.map('map').setView([23.97401041221854, 120.98201232801719], 8)//座標為臺灣地理中心
+const TW_center_coord = [23.97401041221854, 120.98201232801719];
+const TW_center_zoomlv = 7;
+const bTb = document.getElementById('bTb');
+var map = L.map('map').setView(TW_center_coord, TW_center_zoomlv)//座標為臺灣地理中心
+
 
 var div = L.DomUtil.get('bTb'); // this must be an ID, not class!
 L.DomEvent.on(div, 'mousewheel', L.DomEvent.stopPropagation);
 L.DomEvent.on(div, 'click', L.DomEvent.stopPropagation);
+ShowHideBTB();
+map.on('move', (e) => {
+    ShowHideBTB();
+});
+
+
+map.locate();
+map.on('locationfound', (e) => {
+    map.flyTo(e.latlng, 17);
+});
 
 // 預設嚴重性顏色
 const severityColor = {};
@@ -27,7 +41,7 @@ var baselayers = {
 };
 var overlays = {};
 L.control.layers(baselayers, overlays).addTo(map);
-baselayers['CartoDB_Voyager'].addTo(map);
+baselayers['OpenStreetMap.HOT'].addTo(map);
 
 async function GetSheetData() {
     // Google Sheets 分享連結
@@ -56,94 +70,31 @@ async function GetSheetData() {
             cells.forEach(cell => {
                 rowArray.push(cell.textContent.trim());
             });
-            tableArray.push(rowArray);
+            if (rowArray[rowArray.length - 1].length !== 0) //檢查資料最後一欄不為空
+                tableArray.push(rowArray);
         });
-        // 輸出2D陣列
-        for (var h = 3; h < tableArray.length; h++) {
-            var url = tableArray[h][12];
-            if (url != '') {
-                var CAPData = await GetCAPData(url);
-                const parser = new DOMParser();
-                const xmlDoc = parser.parseFromString(CAPData, 'text/xml');
-                const alertNode = xmlDoc.getElementsByTagName('alert')[0];
-                if (alertNode) {
-                    const infoNode = alertNode.getElementsByTagName('info');
-                    for (var i = 0; i < infoNode.length; i++) {
-                        const areaNode = infoNode[i].getElementsByTagName('area');
-                        const severityNode = infoNode[i].getElementsByTagName('severity')[0];
-                        for (var j = 0; j < areaNode.length; j++) {
-                            const circleNode = areaNode[j].getElementsByTagName('circle');
-                            // const polygonNode = areaNode[j].getElementsByTagName('polygon');
-                            if (circleNode.length > 0) {
-                                for (var k = 0; k < circleNode.length; k++) {
-                                    const circleData = circleNode[k].textContent.split(' ');
-                                    const location = circleData[0].split(',');
-                                    const radius = circleData[1] * 1000;
-                                    var circle = L.circle([location[0], location[1]], {
-                                        color: severityColor[severityNode.textContent],
-                                        fillColor: severityColor[severityNode.textContent],
-                                        fillOpacity: 0.1,
-                                        radius: radius
-                                    }).addTo(map);
-                                }
-                            }
-                            // else if (polygonNode.length > 0) {
-                            //     for (var k = 0; k < polygonNode.length; k++) {
-                            //         console.log('polygon：', circleNode[k].textContent);
-                            //         const circleData = circleNode[k].textContent.split(' ');
-                            //         const location = circleData[0].split(',');
-                            //         const radius = circleData[1] * 1000;
-                            //         var circle = L.circle([location[0], location[1]], {
-                            //             color: severityColor[severityNode.textContent],
-                            //             fillColor: severityColor[severityNode.textContent],
-                            //             fillOpacity: 0.1,
-                            //             radius: radius
-                            //         }).addTo(map);
-                            //     }
-                            // }
-                        }
-                    }
-                }
-            }
+
+        for (var i = 0; i < tableArray.length; i++) {
+            console.log(tableArray[i][16])
         }
     } catch (error) {
         console.error('發生錯誤:', error);
     }
 }
 
-function GetCAPData(url) {
-    return new Promise((resolve, reject) => {
-        window.setTimeout(function () {
-            let xhr = new XMLHttpRequest();
-            xhr.open("GET", 'https://corsproxy.io/?' + encodeURIComponent(url), true);// 使用代理伺服器發送請求
-            xhr.onload = function () {
-                if (this.status >= 200 && this.status < 300) {
-                    console.log('成功!URL為: ' + url);
-                    resolve(xhr.response);
-                } else {
-                    console.log('失敗!URL為: ' + url);
-                    reject({
-                        status: xhr.status,
-                        statusText: xhr.statusText
-                    });
-                }
-            }
-            xhr.onerror = function () {
-                reject({
-                    status: this.status,
-                    statusText: xhr.statusText
-                });
-
-            };
-            xhr.send();
-        }, 100);
-    });
-}
 GetSheetData();
 
 function BackToTW() {
-    map.flyTo([23.97401041221854, 120.98201232801719], 8, {
-        animate: true,
-        duration: 1 // in seconds
-    });
+    map.flyTo(TW_center_coord, TW_center_zoomlv);
+}
+
+function ShowHideBTB() {
+    var center = map.getCenter();
+    if (center.lat.toFixed(1) == TW_center_coord[0].toFixed(1) && center.lng.toFixed(1) == TW_center_coord[1].toFixed(1) && map.getZoom() == TW_center_zoomlv) {
+        bTb.classList.remove("visible_anim_up");
+        bTb.classList.add("hidden_anim_down");
+    } else {
+        bTb.classList.remove("hidden_anim_down");
+        bTb.classList.add("visible_anim_up");
+    }
 }
